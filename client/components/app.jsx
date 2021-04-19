22d778d3e2290b367f3b7686bb580dbe1ff5d584
Mason Keiser <masonksr5@gmail.com>
2021-04-19 23:40:20 +0000

    carousel is responsive

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -2,26 +2,13 @@ import React from 'react';
 import HomeCarousel from './carousel'
 
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: null,
-      isLoading: true
-    };
-  }
+const App = () => {
 
-  componentDidMount() {
-    fetch('/api/health-check')
-      .then(res => res.json())
-      .then(data => this.setState({ message: data.message || data.error }))
-      .catch(err => this.setState({ message: err.message }))
-      .finally(() => this.setState({ isLoading: false }));
-  }
-
-  render() {
-    return this.state.isLoading
-      ? <h1>Testing connections...</h1>
-      : <HomeCarousel/>;
-  }
+    return (
+        <div>
+            <HomeCarousel/>
+        </div>
+    )
 }
+
+export default App
