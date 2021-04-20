8332c1bc3c6f7f9b769574221bfea0b288eeb7ac
Mason Keiser <masonksr5@gmail.com>
2021-04-20 03:55:26 +0000

    added padding to search input

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
 import HomeCarousel from './carousel';
 import Nav from './nav'
 
 
 const App = () => {
 
+    const [view, setView] = useState({ name: 'home', params: {}})
+
+    const viewTern = (view.name === 'home')
+        ? <HomeCarousel/>
+        : null
+
     return (
         <div>
-            <Nav/>
-            <HomeCarousel/>
+            <Nav setView={setView}/>
+            {viewTern}
         </div>
     )
 }
