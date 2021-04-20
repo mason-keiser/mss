3f8c886af060bd7060480886870d8a2666779b6c
Mason Keiser <masonksr5@gmail.com>
2021-04-20 03:27:03 +0000

    more styling to nav

--- client/components/nav.jsx
+++ client/components/nav.jsx
@@ -9,17 +9,19 @@ const Nav = (props) => {
                     <img src='/images/icon.png'></img>
                     <h2>Mas's Surf Shop</h2>
                 </div>
-                <div className='inputBtnCont'>
-                    <div className='searchInput'>
-                        <input type="text" placeholder='Search here' id='searchBar'/>
+                <div className='desk'>
+                    <div className='inputBtnCont'>
+                        <div className='searchInput'>
+                            <input type="text" placeholder='Search here' id='searchBar'/>
+                        </div>
+                        <div className='searchBtn'>
+                            <span className='fas fa-search'></span>
+                        </div>
                     </div>
-                    <div className='searchBtn'>
-                        <span className='fas fa-search'></span>
+                    <div className='cartCont'>
+                        <span className='fas fa-shopping-cart'></span>
+                        <h4 className=' mt-1'>0</h4>
                     </div>
-                </div>
-                <div className='cartCont'>
-                    <span className='fas fa-shopping-cart'></span>
-                    <h4 className=' mt-1'>0</h4>
                 </div>
             </div>
             <div className='bottomRow'>
