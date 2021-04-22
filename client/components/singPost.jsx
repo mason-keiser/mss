04d215678af92654005d69d8f30426ae69f5d2bd
Mason Keiser <masonksr5@gmail.com>
2021-04-22 16:58:47 +0000

    add to cart btn created/styled

--- client/components/singPost.jsx
+++ client/components/singPost.jsx
@@ -45,7 +45,7 @@ import { useEffect } from 'react';
             <div className='previewInfo m-2'>
                     <h5>{`$${(price / 100).toFixed(2)}`}</h5>
                     <h4>{props.singPost.name}</h4>
-                    <div>
+                    <div className='btnRow'>
                         <div className='quantityBtn mt-3'>
                             <div>Qty :  
                                 <select className='ml-2' name="" id="selectQty" onChange={handleQtyChange}>
@@ -61,6 +61,7 @@ import { useEffect } from 'react';
                                 </select>
                             </div>
                         </div>
+                        <div className='addToCart mt-3'>Add to cart</div>
                     </div>
                     <h5 className='mt-4'>Description: </h5>
                     <h5 className='mt-2'>{props.singPost.description}</h5>
