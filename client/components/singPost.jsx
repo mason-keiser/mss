d03a214476465eea38957aa68e5858bbf5cd5a6f
Mason Keiser <masonksr5@gmail.com>
2021-04-22 01:52:31 +0000

    started qty select

--- client/components/singPost.jsx
+++ client/components/singPost.jsx
@@ -40,6 +40,23 @@ import { useEffect } from 'react';
             <div className='previewInfo mt-1 ml-3'>
                     <h5>{`$${(price / 100).toFixed(2)}`}</h5>
                     <h4>{props.singPost.name}</h4>
+                    <div>
+                        <div className='quantityBtn'>
+                            <h6>Qty:
+                                <select name="" id="selectQty">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                </select>
+                            </h6>
+                        </div>
+                    </div>
                     <h5 className='mt-4'>Description: </h5>
                     <h5 className='mt-2'>{props.singPost.description}</h5>
             </div>
