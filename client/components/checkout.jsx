25f4184ecb239f5a03dd5d46b5abd8276b9fa986
Mason Keiser <masonksr5@gmail.com>
2021-04-27 01:29:23 +0000

    form created on frontend

--- client/components/checkout.jsx
+++ client/components/checkout.jsx
@@ -18,7 +18,7 @@ import Footer from './footer';
     },[])
 
     return (
-        <div className='container'>
+        <div className='container' style={{height: '80vh'}}>
             <div className='pageTitle'>
                 <h1>Checkout</h1>
             </div>
@@ -27,7 +27,7 @@ import Footer from './footer';
             </div>
 
             <form action="">
-                <div className='wrapper mt-5'>
+                <div className='wrapper mt-3'>
                     <div className='input-data'>
                         <textarea style={{resize: 'none'}}  name="" id="" cols="30" rows="10"></textarea>
                         <label htmlFor="">Email</label>
@@ -40,8 +40,11 @@ import Footer from './footer';
                         <textarea style={{resize: 'none'}} name="" id="addy" cols="30" rows="10"></textarea>
                         <label htmlFor="">Address</label>
                     </div>
-                    <div className='mt-5'>
+                    <div className=''>
                         <input type="checkbox"/><span className='ml-3'>I acknowledge that this is purely for demonstration purposes and no real emails, addresses, or credit card information should be used for the purposes of this app</span>
+                    </div>
+                    <div>
+                        <button className='orderBtn'>Submit Order</button>
                     </div>
                 </div>
             </form>
